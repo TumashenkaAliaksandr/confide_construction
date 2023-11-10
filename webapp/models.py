575092c6563c15de1e73deff8f95c494d6856a8e
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-11-11 00:02:27 +0300

    fixed model News

--- webapp/models.py
+++ webapp/models.py
@@ -64,18 +64,18 @@ class Recommended(models.Model):
 
 class News(models.Model):
     """News Model"""
-    title = models.CharField(max_length=200, verbose_name='Заголовок')
-    description = models.TextField(verbose_name='Описание')
-    location = models.CharField(max_length=200, verbose_name='Место', default='')
-    photo = models.ImageField(upload_to='news_photos/', null=True, blank=True, verbose_name='Фото')
-    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
+    title = models.CharField(max_length=200, verbose_name='Name')
+    description = models.TextField(verbose_name='Descriptions')
+    location = models.CharField(max_length=200, verbose_name='Locations', default='')
+    photo = models.ImageField(upload_to='news_photos/', null=True, blank=True, verbose_name='Photo')
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Data')
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = "Новости"
-        verbose_name_plural = "Новости"
+        verbose_name = "News"
+        verbose_name_plural = "News"
 
     def get_absolute_url(self):
         return reverse('news-datail', kwargs={'pk': self.id})
