0497be6b7c4320f249cddfa142726cc2a4ec8657
kolaider <nikolai.strom@skz.by>
2024-02-19 23:18:12 +0300

    feature: add stripe

--- confide_construction/urls.py
+++ confide_construction/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
     path('', include('blog.urls')),
+    path('payments/', include('payments.urls')),  # new
 ]
