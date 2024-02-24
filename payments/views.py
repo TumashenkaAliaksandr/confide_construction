a5c0faf0a224cba4ce48f5049d1ce830dd78ee6e
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-02-24 18:11:57 +0300

    added new script for sprite from model the disposal

--- payments/views.py
+++ payments/views.py
@@ -26,61 +26,55 @@ def stripe_config(request):
         return JsonResponse(stripe_config, safe=False)
 
 
-@csrf_exempt
 def create_checkout_session(request):
     if request.method == 'GET':
         domain_url = 'http://localhost:8000/'
         stripe.api_key = settings.STRIPE_SECRET_KEY
         try:
-            price = 11
-            disposal = Disposal.objects.get()
-            name = disposal.name
-            descriptions = disposal.description
+            # Получаем первый объект Disposal из базы данных
+            disposal = Disposal.objects.first()
+            if disposal:
+                price = int(disposal.price * 100)  # Преобразуем цену в центы
+                name = disposal.name
+                descriptions = disposal.description
 
-            # Create new Checkout Session for the order
-            # Other optional params include:
-            # [billing_address_collection] - to display billing address details on the page
-            # [customer] - if you have an existing Stripe Customer ID
-            # [payment_intent_data] - capture the payment later
-            # [customer_email] - prefill the email input in the form
-            # For full details see https://stripe.com/docs/api/checkout/sessions/create
-
-
-            # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
-            checkout_session = stripe.checkout.Session.create(
-                success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
-                cancel_url=domain_url + 'cancelled/',
-                payment_method_types=['card'],
-                mode='payment',
-                line_items=[
-                    {
-                        'price_data': {
-                            'currency': 'usd',
-                            'unit_amount': price,
-                            'product_data': {
-                                'name': name,
-                                'description': descriptions,
+                # Создаем новую сессию оформления заказа для товара
+                checkout_session = stripe.checkout.Session.create(
+                    success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
+                    cancel_url=domain_url + 'cancelled/',
+                    payment_method_types=['card'],
+                    mode='payment',
+                    line_items=[
+                        {
+                            'price_data': {
+                                'currency': 'usd',
+                                'unit_amount': price,
+                                'product_data': {
+                                    'name': name,
+                                    'description': descriptions,
+                                },
                             },
+                            'quantity': 1,
                         },
-                        'quantity': 1,
-                    }
-                    ,
-                    {
-                        'price_data': {
-                            'currency': 'usd',
-                            'unit_amount': 2500,  # Цена шорт в центах (25.00 долларов)
-                            'product_data': {
-                                'name': 'Shorts',
-                                'description': 'Sporty shorts for outdoor activities',
+                        {
+                            'price_data': {
+                                'currency': 'usd',
+                                'unit_amount': 15,
+                                'product_data': {
+                                    'name': 'Dimon',
+                                    'description': 'Описание',
+                                },
                             },
-                        },
-                        'quantity': 1,
-                    }
-                ]
-            )
-            return JsonResponse({'sessionId': checkout_session['id']})
+                            'quantity': 1,
+                        }
+                    ]
+                )
+                return JsonResponse({'sessionId': checkout_session['id']})
+            else:
+                return JsonResponse({'error': 'No Disposal objects found'})
         except Exception as e:
             return JsonResponse({'error': str(e)})
+
 
 
 class SuccessView(TemplateView):
