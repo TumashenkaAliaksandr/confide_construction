a3bc9f756f1fa2d501a974459df419a1df0a7c59
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-03-01 13:33:53 +0300

    Renamed the home file in the Payments app, changed the url in the view.

--- payments/urls.py
+++ payments/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('config/', views.stripe_config),
     path('create-checkout-session/', views.create_checkout_session),
     path('success/', views.SuccessView.as_view()),
-    path('cancelled/', views.CancelledView.as_view()),
+    path('cancelled/', views.CancelledView.as_view(), name='home'),
     # path('webhook/', views.stripe_webhook),
 ]
