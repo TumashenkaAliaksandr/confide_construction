0fc6cb54a8eb4d513103cca15984e716fb3236c3
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-06-28 09:24:41 +0300

    moved the statics to the root of the project

--- confide_construction/settings.py
+++ confide_construction/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,9 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = 'static/'
 
-STATIC_URL = '/static/'
+
+STATIC_URL = 'static/'
+
 
 # for django >= 3.1
 STATICFILES_DIRS = [BASE_DIR / 'static']  # new
