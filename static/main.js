eff7dde9fc2759236a713598cbe5a816349a55d0
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-08-26 19:48:07 +0300

    Doing acceleration for the site

--- static/main.js
+++ static/main.js
@@ -3,7 +3,7 @@
 console.log("Sanity check!");
 
 // Get Stripe publishable key
-fetch("config/")
+fetch("/payments/config/")
 .then((result) => { return result.json(); })
 .then((data) => {
   // Initialize Stripe.js
