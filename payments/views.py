a7d8e5946512a1aa079c5ae38c3604463fae3874
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-08-28 23:11:19 +0300

    fixin menu comment my account and checkout

--- payments/views.py
+++ payments/views.py
@@ -77,14 +77,14 @@ class HomePageView(TemplateView):
 
 
 # new
-# @csrf_exempt
+@csrf_exempt
 def stripe_config(request):
     if request.method == 'GET':
         stripe_config = {'publicKey': settings.STRIPE_PUBLISHABLE_KEY}
         return JsonResponse(stripe_config, safe=False)
 
 
-# @csrf_exempt
+@csrf_exempt
 def create_checkout_session(request):
     if request.method == 'GET':
         domain_url = 'http://localhost:8000/'
